85827bf0b1e1dece00aa54807d1dec3c3e008682
seunghun yang <yabby1997@gmail.com>
2021-03-16 13:01:11 +0900

    간단한 컬렉션뷰 만들기

--- FriendsCollection/ViewController.swift
+++ FriendsCollection/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource {
+    // MARK: - IBOutlets
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Properties
+    var numberOfCells: Int = 10
+    let cellIdentifier: String = "cell"
+    
+    // MARK: - View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.collectionView.dataSource = self
     }
 
-
+    // MARK: - CollectionViewDataSource Methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfCells
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        
+        return cell
+    }
 }
 
